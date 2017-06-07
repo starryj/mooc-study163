e894f3463937a9b4588b1cf071fd0bed1666cc5b
starryj <276694730@qq.com>
2017-06-07 19:48:48 +0800

    Update DownLoad.py

--- DownLoad.py
+++ DownLoad.py
@@ -18,47 +18,53 @@ from progressbar import ProgressBar
 from multiprocessing import Pool
 
 
-class CourseDown(object):
+class MoocDown(object):
     def __init__(self):
         self.client = pymongo.MongoClient('localhost')
         self.db_mooc = self.client['MOOC']
         self.db_study = self.client['study163']
 
     @staticmethod
-    def videoDown(video_name, video_href, video_id, video_type, td):
-        path1 = os.path.join('D:\DATAS\MOOC Videos\\', video_name)
-        if not os.path.exists(path1):
-            os.mkdir(path1)
+    def videoDown(class_name, video_href, video_type, td, detail_name):
+        path = os.path.join('D:\DATAS\MOOC Videos\\', class_name)
+        if not os.path.exists(path):
+            os.mkdir(path)
         if td == '0':
             cl = 'MOOC'
-            path = os.path.join(path1, video_name + str(video_id) + '.' + video_type)
+            week_name, until_name, course_name = detail_name
+            path0 = os.path.join(path, week_name)
+            if not os.path.exists(path0):
+                os.mkdir(path0)
+            path1 = os.path.join(path0, until_name)
+            if not os.path.exists(path1):
+                os.mkdir(path1)
+            video_path = os.path.join(path1, course_name + '.' + video_type)
         else:
             cl = 'study163'
-            path = os.path.join(path1, str(video_id) + '.' + video_type)
+            course_name = detail_name
+            video_path = os.path.join(path, str(course_name) + '.' + video_type)
         header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'}
-        if os.path.exists(path):
-            os.remove(path)
         try:
             with closing(requests.get(url=video_href, headers=header, stream=True)) as response:
                 count_size = int(response.headers['Content-Length'])
                 chunk_size = 1024*4
-                p = ProgressBar(total=count_size, name=video_name + str(video_id))
+                p = ProgressBar(total=count_size, name=course_name)
                 for i in response.iter_content(chunk_size):
-                    with open(path, 'ab') as f:
+                    with open(video_path, 'ab') as f:
                         f.write(i)
                     p.log(len(i))
             client = pymongo.MongoClient('localhost')
             db = client[cl]
-            db[video_name].remove({'_id': video_id})
+            db[class_name].remove({'course_name': course_name})
             client.close()
         except Exception as E:
             print(E)
 
     def mooc(self, video='超清flv'):
-        href = []
-        ids = []
-        p_href = []
-        p_ids = []
+        video_href = []
+        pdf_href = []
+        v_name = []
+        p_name = []
         class_name = input("输入你正在找的课程(Mooc)：")
 
         coll = self.db_mooc[class_name]
@@ -73,28 +79,34 @@ class CourseDown(object):
             P_links = coll.find({'name': 'pdf'})
             T_texts = coll.find({'name': 'text'})
         vt = video[2:]
-        for link in V_links:
-            href.append(link[video])
-            ids.append(link['_id'])
-        for P_link in P_links:
-            p_href.append(P_link['pdf'])
-            p_ids.append(P_link['_id'])
+        for v in V_links:
+            video_href.append(v[video])
+            v_name.append((v['week_name'], v['until_name'], v['course_name']))
+        for p in P_links:
+            pdf_href.append(p['pdf'])
+            p_name.append((p['week_name'], p['until_name'], p['course_name']))
         path1 = os.path.join('D:\DATAS\MOOC Videos\\', class_name)
         if not os.path.exists(path1):
             os.mkdir(path1)
         print('正在下载富文本文件')
-        for texts in T_texts:
-            text_id = texts['_id']
-            path = os.path.join(path1, class_name + str(text_id) + '.txt')
-            with open(path, 'w', encoding='utf-8') as f:
-                f.write(texts['text'])
-                print('成功下载富文本文件----' + str(text_id))
+        for t in T_texts:
+            print(t)
+            path0 = os.path.join(path1, t['week_name'])
+            if not os.path.exists(path0):
+                os.mkdir(path0)
+            path1 = os.path.join(path0, t['until_name'])
+            if not os.path.exists(path1):
+                os.mkdir(path1)
+            t_path = os.path.join(path1, t['course_name'] + '.py')
+            with open(t_path, 'w', encoding='utf-8') as f:
+                f.write(t['text'].replace(r'\n', '\n'))
+                print('成功下载富文本文件----' + str(t['course_name']))
         self.client.close()
         pool1 = Pool(4)
         try:
             print('正在下载视频课件')
-            for h in range(len(href)):
-                pool1.apply_async(func=self.videoDown, args=(class_name, href[h], ids[h], vt, '0'))
+            for h in range(len(video_href)):
+                pool1.apply_async(func=self.videoDown, args=(class_name, video_href[h], vt, '0', v_name[h]))
             pool1.close()
             pool1.join()
         except Exception as e:
@@ -102,8 +114,8 @@ class CourseDown(object):
         pool2 = Pool(4)
         try:
             print('正在下载pdf课件')
-            for p in range(len(p_href)):
-                pool2.apply_async(func=self.videoDown, args=(class_name, p_href[p], p_ids[p], 'pdf', '0'))
+            for p in range(len(pdf_href)):
+                pool2.apply_async(func=self.videoDown, args=(class_name, pdf_href[p], 'pdf', '0', p_name[p]))
             pool2.close()
             pool2.join()
         except Exception as e:
@@ -128,12 +140,12 @@ class CourseDown(object):
             self.client.close()
         for link in links:
             href.append(link[video])
-            course_name.append(link['_id'])
+            course_name.append(link['course_name'])
         pool = Pool(4)
         try:
             print('正在下载视频课件')
             for h in range(len(href)):
-                pool.apply_async(func=self.videoDown, args=(class_name, href[h], course_name[h], vt, '1'))
+                pool.apply_async(func=self.videoDown, args=(class_name, href[h], vt, '1', course_name[h]))
             pool.close()
             pool.join()
         except Exception as e:
@@ -151,5 +163,5 @@ class CourseDown(object):
             print('ERROR')
 
 if __name__ == '__main__':
-    mooc = CourseDown()
+    mooc = MoocDown()
     mooc.download(where=0)
